a063954f7c546bb92d9a2c95e6fc868e5ccc69fd
limaaaaa0 <162451541+limaaaaa0@users.noreply.github.com>
2025-02-24 08:46:57 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,19 @@
+// Operadores de comparação 
+// Operaor de igualdade (compara apenas o valor)
+console.log("1" == 1)
+console.log(true == 1)
+console.log(false == 0)
+console.log(false == "")
+
+//Operador de igualdade estrita 
+// (compara o valor e tipo)
+
+console.log("1" === 1)
+console.log(true === 1)
+console.log(false === 0)
+console.log(false === "")
+ 
+// verificar  tipo de variável
+console.log(typeof("1"))// -> texto (string)
+console.log(typeof(1))// -> Number
+console.log(typeof(true))// -> boolean
---
+++ calculadora.js
@@ -0,0 +1,12 @@
+let valor1 = 2
+let valor2 = 4
+
+let soma = valor1 + valor2
+let sub = valor1 - valor2
+let mult = valor1 * valor2
+let div = valor1 / valor2
+
+console.log("soma = " + soma)
+console.log("subtração = " + sub)
+console.log("multiplicação = " + mult) 
+console.log("divisão = " + div)
