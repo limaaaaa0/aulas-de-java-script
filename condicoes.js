1775adc4972788dd79a0d1f175bd4e80e4ac4eb8
limaaaaa0 <162451541+limaaaaa0@users.noreply.github.com>
2025-02-25 12:08:28 -0300

    Add files via upload

---
+++ condicoes.js
@@ -0,0 +1,11 @@
+let idade = 16
+let possuiCNH = false
+if (idade >= 18){
+    if (possuiCNH == true) {
+        console.log("Você pode dirigir")
+    } else {
+        console.log("Você pode tirar CNH")
+    }
+} else {
+    console.log("Não pode")
+}
---
+++ exercicio.js
@@ -0,0 +1,7 @@
+let peso = 70  
+
+let agua = peso * 35
+
+console.log("multiplicação = " + agua) 
+
+// 70 x 35 = 2450ml -> 2L e 450 ml
